a21776ec4d6b68ea0fbe6cd7d8989f6654bc8d8f
ponxu <xwz689@gmail.com>
2014-08-12 17:44:31 +0800

    add post

---
+++ _posts/编程语言/Python/python-non-blocking-socket.py
@@ -0,0 +1,18 @@
+import socket
+s = socket.socket()
+s.setblocking(0)
+
+try:
+    s.connect(('www.baidu.com', 80))
+except socket.error as e:
+    print(str(e))
+    i = 0
+    while True:
+        try:
+            print("We are connected to %s:%d" % s.getpeername())
+            break
+        except:
+            print("Let's do some math while waiting: %d" % i)
+            i += 1
+else:
+    print("We are connected to %s:%d" % s.getpeername())
